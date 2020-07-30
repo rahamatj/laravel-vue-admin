e5617c23d3ee288b8813db90cb223bbc969e8edb
Rahamat Jahan <rahamatj92@gmail.com>
2020-07-30 23:03:39 +0600

    Client side route gurad bug fixed

--- client/src/guards/index.js
+++ client/src/guards/index.js
@@ -3,38 +3,38 @@ import store from '@/store'
 export default {
   authenticate: (to, from, next) => {
     if (store.getters['login/isAuthenticated'])
-      next({ name: 'dashboard' })
+      return next({ name: 'dashboard' })
 
-    next()
+    return next()
   },
   accessApp: (to, from, next) => {
     if (! store.getters['login/isAuthenticated'])
-      next({ name: 'login' })
+      return next({ name: 'login' })
 
     if (store.getters['login/user'].is_otp_verification_enabled_at_login
       && ! store.getters['checkpoint/isOtpVerifiedAtLogin'])
-      next({ name: 'checkpoint' })
+      return next({ name: 'checkpoint' })
 
-    next()
+    return next()
   },
   verifyOtpAtLogin: (to, from, next) => {
     const user = store.getters['login/user']
 
     if (user.otp_type === 'google2fa' && ! user.is_google2fa_activated)
-      next({ name: 'google2fa.activate' })
+      return next({ name: 'google2fa.activate' })
 
     if (! store.getters['login/user'].is_otp_verification_enabled_at_login
       || store.getters['checkpoint/isOtpVerifiedAtLogin'])
-      next({ name: 'dashboard' })
+      return next({ name: 'dashboard' })
 
-    next()
+    return next()
   },
   activateGoogle2fa: (to, from, next) => {
     const user = store.getters['login/user']
 
     if (user.is_google2fa_activated || user.otp_type !== 'google2fa')
-      next({ name: 'checkpoint' })
+      return next({ name: 'checkpoint' })
 
-    next()
+    return next()
   }
 }
